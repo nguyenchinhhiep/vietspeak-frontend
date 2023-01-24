41cf7582879de27de05578c50c3c46f65bf9e475
Hiep Nguyen <nguyenchinhhiep95@gmail.com>
2023-01-24 16:01:13 +0700

    update something new

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,7 @@ import { LanguageCode } from 'src/app/core/translation/language.model';
 export const environment = {
   production: false,
   projectName: 'vietspeak',
-  serverUrl: 'http://localhost:1996/api/',
+  serverUrl: 'https://vietspeak-backend.vercel.app/api/',
   supportedLanguages: [LanguageCode.English, LanguageCode.Vietnamese],
   defaultLanguage: LanguageCode.Vietnamese,
 };
